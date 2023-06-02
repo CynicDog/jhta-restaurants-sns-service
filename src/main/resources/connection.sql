53c2bc918656d0904fdf51b90d2c384b38b03cac
ginsenglee <thecynicdog0328@gmail.com>
2023-06-02 14:56:14 +0900

    db username modified

--- src/main/resources/connection.sql
+++ src/main/resources/connection.sql
@@ -1,3 +1,8 @@
-// connection name: jhta_restaurants_sns_service
-// username: jhta
-// password: 1234
+-- connection name: jhta_restaurants_sns_service
+-- username: jhta
+-- password: 1234
+
+CREATE USER C##jhta IDENTIFIED BY 1234;
+GRANT RESOURCE, CONNECT TO C##jhta;
+grant create session, create table, create procedure to C##jhta;
+ALTER USER C##jhta quota unlimited on USERS;
