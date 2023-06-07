2c75b468f37ac960786d0c7ff9ea2cd883607b3d
sungbae <wdddd9@naver.com>
2023-06-07 15:04:23 +0900

    sungbae_StoreDao_staticAndGetById

--- src/main/java/dao/StoreDao.java
+++ src/main/java/dao/StoreDao.java
@@ -1,5 +1,35 @@
 package dao;
 
+import utils.DaoHelper;
+import vo.Owner;
+import vo.Store;
+
 public class StoreDao {
 
+	private static StoreDao instance = new StoreDao();
+	private StoreDao() {}
+	
+	public static StoreDao getInstance() {
+		return instance;
+	}
+	
+	public Store getStoreById(int storeId) { 
+		return DaoHelper.selectOne("StoreDao.getStoreById", rs->{
+			Owner owner = new Owner();
+			owner.setId(rs.getInt(8));
+			Store store = new Store(
+					rs.getInt(1), 
+					rs.getInt(2),
+					rs.getString(3),
+					rs.getInt(4),
+					rs.getFloat(5),
+					rs.getFloat(6),
+					rs.getString(7),
+					owner
+					);
+
+			return store; 
+		}, storeId);
+	}
+	
 }
