1b4e2749b462ec50ff0d966af8ad3a6ad7f4b271
jisung <sjs970808@naver.com>
2023-06-07 10:54:09 +0900

    CustomerVo

--- src/main/java/vo/Customer.java
+++ src/main/java/vo/Customer.java
@@ -1,35 +1,117 @@
 package vo;
 
+import java.util.Date;
 
 public class Customer {
 
-	private int id; 	// auto_increment  
+	private int id;
+	private String user_id;
+	private String user_password;
+	private String user_name;
+	private String user_email;
+	private String user_phone;
+	private Date user_birthday;
+	private String user_gender;
+	private String user_grade;
+	private Date user_create_date;
+	private Date user_update_date;
+	private int review_no;
 	
-	private String name;
+	public  Customer() {}
 
-	
-	public Customer() {}
-	
-	// for storing 
-	public Customer(String name) {
-		this.name = name; 
-	}
-	
-	// for fetching 
-	public Customer(int id, String name) {
-		this.id = id; 
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 	public int getId() {
-		return id; 
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public String getUser_phone() {
+		return user_phone;
+	}
+
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+
+	public Date getUser_birthday() {
+		return user_birthday;
+	}
+
+	public void setUser_birthday(Date user_birthday) {
+		this.user_birthday = user_birthday;
+	}
+
+	public String getUser_gender() {
+		return user_gender;
+	}
+
+	public void setUser_gender(String user_gender) {
+		this.user_gender = user_gender;
+	}
+
+	public String getUser_grade() {
+		return user_grade;
+	}
+
+	public void setUser_grade(String user_grade) {
+		this.user_grade = user_grade;
+	}
+
+	public Date getUser_create_date() {
+		return user_create_date;
+	}
+
+	public void setUser_create_date(Date user_create_date) {
+		this.user_create_date = user_create_date;
+	}
+
+	public Date getUser_update_date() {
+		return user_update_date;
+	}
+
+	public void setUser_update_date(Date user_update_date) {
+		this.user_update_date = user_update_date;
+	}
+
+	public int getReview_no() {
+		return review_no;
+	}
+
+	public void setReview_no(int review_no) {
+		this.review_no = review_no;
 	}
 }
