b9cee97cf21cc1dc2b7f253cc343d7ffdd67c5da
ginsenglee <thecynicdog0328@gmail.com>
2023-06-07 14:11:42 +0900

    class renamed

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -3,9 +3,9 @@ package dao;
 import utils.DaoHelper;
 import vo.Customer;
 
-public class CustomerDAO {
+public class CustomerDao {
 
-	public void insertCustomer(Customer customer) {
+	public static void insertCustomer(Customer customer) {
 		
 		DaoHelper.update("CustomerDAO.insertCustomer", 
 					customer.getName()
