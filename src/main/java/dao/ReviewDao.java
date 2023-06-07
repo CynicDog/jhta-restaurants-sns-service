97ed869b0ca6aef8b9d5c5336a2483b19ad710ec
jisung <sjs970808@naver.com>
2023-06-07 15:15:38 +0900

    dao update

--- src/main/java/dao/ReviewDao.java
+++ src/main/java/dao/ReviewDao.java
@@ -1,5 +1,22 @@
 package dao;
 
+import utils.DaoHelper;
+import vo.Review;
+
 public class ReviewDao {
 
+	public Review getReviewById(int id) {
+		return DaoHelper.selectOne("", rs -> {
+			Review review = new Review(
+					rs.getInt(1),
+					rs.getInt(2),
+					rs.getString(3),
+					rs.getDate(4),
+					rs.getDate(5),
+					rs.getInt(6),
+					rs.getInt(7));
+			
+			return review;
+		}, id);
+	}
 }
