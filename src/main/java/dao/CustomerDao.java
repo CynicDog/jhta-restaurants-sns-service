aebd1df83e2b26a4b5904911530da4fd5e99f457
sojung <sjleee406@naver.com>
2023-06-07 15:29:00 +0900

    sojung_customerDao

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -2,14 +2,51 @@ package dao;
 
 import utils.DaoHelper;
 import vo.Customer;
+import vo.Owner;
 
 public class CustomerDao {
 
 	public void insertCustomer(Customer customer) {
 		
 		DaoHelper.update("CustomerDao.insertCustomer", 
-					customer.getName()
+					customer.getName(),
+					customer.getUserId(),
+					customer.getPassword(),
+					customer.getEmail(),
+					customer.getPhone(),
+					customer.getBirthday(),
+					customer.getGender(),
 				);
 	} 
+	
+	public void updateCustomer(Customer customer) {
+		DaoHelper.update("customerDao.updateCustomer",
+					customer.getUserId(),
+					customer.getPassword(),
+					customer.getEmail(),
+					customer.getPhone(),
+					customer.getGrade(),
+				);
+	}
+	
+	public Customer getCustomerById(int customerId) {
+		return DaoHelper.selectOne("OwnerDao.getCustomerById", rs->{
+			Customer customer = new Customer(
+					rs.getInt(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getString(5),
+					rs.getString(6),
+					rs.getDate(7),
+					rs.getString(8),
+					rs.getString(9),
+					rs.getDate(10),
+					rs.getDate(11));
+
+			return customer; 
+			
+		}, customerId);
+	}
 	
 }
