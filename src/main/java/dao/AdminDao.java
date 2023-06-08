a8e6efb25f5ee7f5ca90cf4723fc58b5089a272c
GoTaeHwa <koteha@naver.com>
2023-06-08 15:58:42 +0900

    adminDao, StoreOpenTimeDao / getbyName update

--- src/main/java/dao/AdminDao.java
+++ src/main/java/dao/AdminDao.java
@@ -32,6 +32,25 @@ public class AdminDao {
 		}, id);
 	}
 	
+	public Admin getAdminByName(String name) {
+		return DaoHelper.selectOne("AdminDao.getAdminByName", rs -> {
+			Admin admin = new Admin();
+			
+			admin.setId(rs.getInt(1));
+			admin.setAdminId(rs.getString(2));
+			admin.setPassword(rs.getString(3));
+			admin.setName(rs.getString(4));
+			admin.setEmail(rs.getString(5));
+			admin.setPhone(rs.getString(6));
+			admin.setBirthday(rs.getDate(7));
+			admin.setGender(rs.getString(8));
+			admin.setCreateDate(rs.getDate(9));
+			admin.setUpdateDate(rs.getDate(10));
+		
+			return admin;
+		}, name);
+	}
+	
 	public void insertAdmin(Admin admin) {
 		DaoHelper.update("adminDao.insertAdmin",
 								admin.getAdminId(),
@@ -53,5 +72,13 @@ public class AdminDao {
 				admin.getBirthday(),
 				admin.getGender(),
 				admin.getUpdateDate());
+	}
+	
+	public void deleteAdminById(int id) {
+		DaoHelper.update("AdminDao.deleteAdminById", id);
+	}
+	
+	public void deleteAdminByName(String name) {
+		DaoHelper.update("AdminDao.deleteAdminByName", name);
 	}
 }
