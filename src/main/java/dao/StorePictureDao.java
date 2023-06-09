ac3d5f8268815db186120bc3e36e9da276a8d493
jisung <sjs970808@naver.com>
2023-06-09 11:10:17 +0900

    storepicture dao, properties upodate

---
+++ src/main/java/dao/StorePictureDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import utils.DaoHelper;
+import vo.Store;
+import vo.StorePicture;
+
+public class StorePictureDao {
+
+	StoreDao storeDao = StoreDao.getInstance();
+	
+	private static StorePictureDao instance = new StorePictureDao();
+	private StorePictureDao() {}
+	public static StorePictureDao getInstance() {
+		return instance;
+	}
+	
+	public StorePicture getStorePictureById(int id) {
+		return DaoHelper.selectOne("StorePictureDao.getStorePictureById", rs -> {
+			StorePicture storePicture = new StorePicture();
+			
+			storePicture.setId(rs.getInt(1));
+			storePicture.setFileLocation(rs.getString(2));
+			
+			Store store = storeDao.getStoreById(rs.getInt(3)); 
+			storePicture.setStore(store);
+			
+			return storePicture;
+		}, id);
+	}
+	
+	public void insertStorePicture(StorePicture storePicture) {
+		DaoHelper.update("StoreOpenTimeDao.insertStoreOpenTime",
+				storePicture.getFileLocation(),
+				storePicture.getStore().getId());
+	}
+	
+	public void updateStorePicture(StorePicture storePicture) {
+		DaoHelper.update("StorePictureDao.updateStorePicture",
+			storePicture.getFileLocation(),
+			storePicture.getStore().getId(),
+			storePicture.getId());
+	}
+	
+	public void deleteStorePicture(int id) {
+		DaoHelper.update("StorePicture.deleteStorePictureById", id);
+	}
+}
