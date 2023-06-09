d304a704fe811d9524e395c6a90d0add88b6a38e
GoTaeHwa <koteha@naver.com>
2023-06-09 10:37:08 +0900

    foodPictureDao created

---
+++ src/main/java/dao/FoodPictureDao.java
@@ -0,0 +1,50 @@
+package dao;
+
+import oracle.security.crypto.core.RSA;
+import utils.DaoHelper;
+import vo.Food;
+import vo.FoodPicture;
+
+public class FoodPictureDao {
+	
+	FoodDao foodDao = FoodDao.getInstance();
+	
+	private static FoodPictureDao instance = new FoodPictureDao();
+	private FoodPictureDao() {}
+	public static FoodPictureDao getInstance() {
+		return instance;
+	}
+	
+	public FoodPicture getFoodPictureById(int id) {
+		
+		return DaoHelper.selectOne("", rs->{
+			FoodPicture foodPicture = new FoodPicture();
+			
+			foodPicture.setId(rs.getInt(1));
+			foodPicture.setFileLocation(rs.getString(2));
+
+			Food food = foodDao.getFoodById(rs.getInt(3));
+			foodPicture.setFood(food);
+			
+			return foodPicture;
+		}, id);
+	}
+	
+	public void insertFoodPicture(FoodPicture foodPicture) {
+		DaoHelper.update("",
+				foodPicture.getFileLocation(),
+				foodPicture.getFood().getId());
+	}
+	
+	public void updateFoodPicture(FoodPicture foodPicture) {
+		DaoHelper.update("", 
+				foodPicture.getFileLocation(),
+				foodPicture.getFood().getId(),
+				foodPicture.getId());
+		
+	}
+	
+	public void deleteFoodPictureById(int id) {
+		DaoHelper.update("", id);
+	}
+}
