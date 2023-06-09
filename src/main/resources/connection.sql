558ce2671b2a298d679b1fd8b85d6571ffa64c40
ginsenglee <thecynicdog0328@gmail.com>
2023-06-09 11:03:21 +0900

    sql

--- src/main/resources/connection.sql
+++ src/main/resources/connection.sql
@@ -1,7 +1,3 @@
--- connection name: jhta_restaurants_sns_service
--- username: jhta
--- password: 1234
-
 CREATE USER C##jhta IDENTIFIED BY 1234;
 GRANT RESOURCE, CONNECT TO C##jhta;
 grant create session, create table, create procedure to C##jhta;
---
+++ src/main/resources/sequences.sql
@@ -0,0 +1,39 @@
+create sequence customers_sequence 
+increment by 1 
+start with 1;
+
+create sequence admins_sequence
+increment by 1 
+start with 5001;
+
+create sequence foods_sequence 
+increment by 1 
+start with 10001;
+
+create sequence food_pictures_sequence 
+increment by 1 
+start with 15001;
+
+create sequence owners_sequence 
+increment by 1 
+start with 20001;
+
+create sequence reviews_sequence 
+increment by 1 
+start with 25001;
+
+create sequence reviews_pictures_sequence 
+increment by 1 
+start with 30001;
+
+create sequence stores_sequence
+increment by 1 
+start with 35001;
+
+create sequence store_open_times_sequence
+increment by 1 
+start with 40001;
+
+create sequence store_pictures_sequence
+increment by 1 
+start with 45001;
