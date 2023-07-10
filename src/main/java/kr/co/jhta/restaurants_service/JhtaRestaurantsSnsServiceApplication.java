280f7a9374fc02fcfa5f6e974777fbc966156171
ginsenglee <thecynicdog0328@gmail.com>
2023-07-10 17:19:03 +0900

    init

---
+++ src/main/java/kr/co/jhta/restaurants_service/JhtaRestaurantsSnsServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.restaurants_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhtaRestaurantsSnsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhtaRestaurantsSnsServiceApplication.class, args);
+    }
+
+}
