2e1f198ee4cf1403c52cf3f35a41b67363362423
ginsenglee <thecynicdog0328@gmail.com>
2023-08-03 17:10:40 +0900

    assigned alias

--- src/main/java/kr/co/jhta/restaurants_service/vo/Follows.java
+++ src/main/java/kr/co/jhta/restaurants_service/vo/Follows.java
@@ -4,15 +4,27 @@ package kr.co.jhta.restaurants_service.vo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.ibatis.type.Alias;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Alias("Follows")
 public class Follows {
 
 	private int followerId;
 	private int followedId;
+
+	@CreatedDate
 	private Date createDate;
+
+	public Follows() {
+	}
+
+	public Follows(int followerId, int followedId) {
+		this.followerId = followerId;
+		this.followedId = followedId;
+	}
 }
