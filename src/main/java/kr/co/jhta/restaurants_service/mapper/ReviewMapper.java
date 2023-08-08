811c6b3caed464671292714fe6a1e1e4eae72c81
sjlee <sjlee@BOOK-QH4OB1UE3E>
2023-08-08 21:54:08 +0900

    sojung_review

--- src/main/java/kr/co/jhta/restaurants_service/mapper/ReviewMapper.java
+++ src/main/java/kr/co/jhta/restaurants_service/mapper/ReviewMapper.java
@@ -3,11 +3,13 @@ package kr.co.jhta.restaurants_service.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.jhta.restaurants_service.vo.review.Review;
+import kr.co.jhta.restaurants_service.vo.store.Store;
 
 @Mapper
 public interface ReviewMapper {
 
-	Review getReviewByNo(int id);
-	Review getReviewByUserId(int customerId);
+	void insertReview(Review review);
+	Review getReviewById(int id);
+	Review getReviewByCustomerId(int customerId);
 	
 }
