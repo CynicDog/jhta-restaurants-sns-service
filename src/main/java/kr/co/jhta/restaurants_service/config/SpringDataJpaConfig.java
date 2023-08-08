780ef8db3f6b03b605aa286520aedee1af303b31
ginsenglee <thecynicdog0328@gmail.com>
2023-08-08 16:03:55 +0900

    dependency / config

---
+++ src/main/java/kr/co/jhta/restaurants_service/config/SpringDataJpaConfig.java
@@ -0,0 +1,65 @@
+package kr.co.jhta.restaurants_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "kr.co.jhta.restaurants_service.repository")
+public class SpringDataJpaConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://database-1.csebivwnfevt.us-east-2.rds.amazonaws.com:3306/database1");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("zxcv1234");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setShowSql(true);
+
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+
+        localContainerEntityManagerFactoryBean.setJpaProperties(properties);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        localContainerEntityManagerFactoryBean.setPackagesToScan("kr.co.jhta.restaurants_service");
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return jpaTransactionManager;
+    }
+}
