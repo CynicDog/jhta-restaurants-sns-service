b167e416123d7508e2b03eefc35b76faec570bbc
ginsenglee <thecynicdog0328@gmail.com>
2023-08-15 20:50:14 +0900

    store insertion

--- src/main/java/kr/co/jhta/restaurants_service/controller/users/OwnerController.java
+++ src/main/java/kr/co/jhta/restaurants_service/controller/users/OwnerController.java
@@ -6,6 +6,8 @@ import kr.co.jhta.restaurants_service.controller.command.StoreOpenTimeCommand;
 import kr.co.jhta.restaurants_service.controller.command.UserCommand;
 import kr.co.jhta.restaurants_service.security.domain.SecurityUser;
 import kr.co.jhta.restaurants_service.security.service.UserService;
+import kr.co.jhta.restaurants_service.service.StoreService;
+import kr.co.jhta.restaurants_service.vo.store.Store;
 import org.jboss.logging.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,12 +29,23 @@ import java.util.stream.Collectors;
 @RequestMapping("/owner")
 public class OwnerController {
 
+    public static String[] PUBLIC_URLS = {"/owner/signup"};
     private final Logger logger = Logger.getLogger(UserController.class);
     private final UserService userService;
+    private final StoreService storeService;
 
-
-    public OwnerController(UserService userService) {
+    public OwnerController(UserService userService, StoreService storeService) {
         this.userService = userService;
+        this.storeService = storeService;
+    }
+
+    @ResponseBody
+    @GetMapping("/store-categories")
+    private ResponseEntity<List<String>> categories() {
+
+        List<String> categoryNames = Arrays.stream(Store.CATEGORY.values()).map(Enum::name).collect(Collectors.toList());
+
+        return ResponseEntity.ok(categoryNames);
     }
 
     @PostMapping("/register-hour")
@@ -41,30 +55,22 @@ public class OwnerController {
         List<String> registeredDays = Optional.ofNullable((List<String>) httpSession.getAttribute("registeredDays")).orElse(new ArrayList<>());
 
         // filter out non-ongoing days
-        List<String> toBeRegistered = clickedDays.stream()
-                .filter(day -> !registeredDays.contains(day))
-                .collect(Collectors.toList());
+        List<String> toBeRegistered = clickedDays.stream().filter(day -> !registeredDays.contains(day)).collect(Collectors.toList());
 
         registeredDays.addAll(toBeRegistered);
 
         // for flushing resource server
-        List<StoreOpenTimeCommand> registeredStoreOpenTimes =
-                registeredDays.stream()
-                        .map(day -> {
-                            return new StoreOpenTimeCommand(day, storeOpenTimeCommand.getOpeningTime(), storeOpenTimeCommand.getClosingTime());
-                        })
-                        .collect(Collectors.toList());
+        List<StoreOpenTimeCommand> registeredStoreOpenTimes = registeredDays.stream().map(day -> {
+            return new StoreOpenTimeCommand(day, storeOpenTimeCommand.getOpeningTime(), storeOpenTimeCommand.getClosingTime());
+        }).collect(Collectors.toList());
 
         httpSession.setAttribute("registeredStoreOpenTimes", registeredStoreOpenTimes);
         httpSession.setAttribute("registeredDays", registeredDays);
 
         // for flushing client representation
-        List<StoreOpenTimeCommand> toBeRegisteredStoreOpenTimeCommands =
-                toBeRegistered.stream()
-                        .map(day -> {
-                            return new StoreOpenTimeCommand(day, storeOpenTimeCommand.getOpeningTime(), storeOpenTimeCommand.getClosingTime());
-                        })
-                        .collect(Collectors.toList());
+        List<StoreOpenTimeCommand> toBeRegisteredStoreOpenTimeCommands = toBeRegistered.stream().map(day -> {
+            return new StoreOpenTimeCommand(day, storeOpenTimeCommand.getOpeningTime(), storeOpenTimeCommand.getClosingTime());
+        }).collect(Collectors.toList());
 
         return new ResponseEntity<>(toBeRegisteredStoreOpenTimeCommands, HttpStatus.OK);
     }
@@ -88,19 +94,13 @@ public class OwnerController {
         List<String> registeredDays = Optional.ofNullable((List<String>) httpSession.getAttribute("registeredDays")).orElse(new ArrayList<>());
         List<StoreOpenTimeCommand> registeredStoreOpenTimes = Optional.ofNullable((List<StoreOpenTimeCommand>) httpSession.getAttribute("registeredStoreOpenTimes")).orElse(new ArrayList<>());
 
-        clickedDays = clickedDays.stream()
-                .filter(day -> !toBeRemoved.equals(day))
-                .collect(Collectors.toList());
+        clickedDays = clickedDays.stream().filter(day -> !toBeRemoved.equals(day)).collect(Collectors.toList());
         httpSession.setAttribute("clickedDays", clickedDays);
 
-        registeredDays = registeredDays.stream()
-                .filter(day -> !toBeRemoved.equals(day))
-                .collect(Collectors.toList());;
+        registeredDays = registeredDays.stream().filter(day -> !toBeRemoved.equals(day)).collect(Collectors.toList());
         httpSession.setAttribute("registeredDays", registeredDays);
 
-        registeredStoreOpenTimes = registeredStoreOpenTimes.stream()
-                .filter(openTime -> !toBeRemoved.equals(openTime.getDay()))
-                .collect(Collectors.toList());
+        registeredStoreOpenTimes = registeredStoreOpenTimes.stream().filter(openTime -> !toBeRemoved.equals(openTime.getDay())).collect(Collectors.toList());
 
         httpSession.setAttribute("registeredStoreOpenTimes", registeredStoreOpenTimes);
 
@@ -123,8 +123,7 @@ public class OwnerController {
 
         List<FoodCommand> foods = Optional.ofNullable((List<FoodCommand>) httpSession.getAttribute("foods")).orElse(new ArrayList<>());
 
-        return foods
-                .stream()
+        return foods.stream()
                 .map(FoodCommand::getFoodName)
                 .anyMatch(foodName -> toBeChecked.equals(foodName)) ?
                 ResponseEntity.badRequest().body("Already exists :(") :
@@ -136,9 +135,7 @@ public class OwnerController {
 
         List<FoodCommand> foods = Optional.ofNullable((List<FoodCommand>) httpSession.getAttribute("foods")).orElse(new ArrayList<>());
 
-        foods = foods.stream()
-                .filter(food -> !toBeRemoved.equals(food.getFoodName()))
-                .collect(Collectors.toList());
+        foods = foods.stream().filter(food -> !toBeRemoved.equals(food.getFoodName())).collect(Collectors.toList());
 
         httpSession.setAttribute("foods", foods);
 
@@ -157,11 +154,21 @@ public class OwnerController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity registerStore(@RequestBody StoreCommand storeCommand, HttpSession httpSession) {
+    public ResponseEntity registerStore(@RequestBody StoreCommand storeCommand,
+                                        @AuthenticationPrincipal SecurityUser securityUser,
+                                        HttpSession httpSession) {
 
+        List<FoodCommand> foods = Optional.ofNullable((List<FoodCommand>) httpSession.getAttribute("foods")).orElse(new ArrayList<>());
+        List<StoreOpenTimeCommand> storeOpenTimes = Optional.ofNullable((List<StoreOpenTimeCommand>) httpSession.getAttribute("registeredStoreOpenTimes")).orElse(new ArrayList<>());
 
+        storeService.insertStore(storeCommand, foods, storeOpenTimes, securityUser);
 
-        return null;
+        httpSession.setAttribute("foods", null);
+        httpSession.setAttribute("registeredStoreOpenTimes", null);
+        httpSession.setAttribute("registeredDays", null);
+        httpSession.setAttribute("clickedDays", null);
+
+        return ResponseEntity.ok().body("Successfully registered!");
     }
 
     @GetMapping("/register")
@@ -191,8 +198,4 @@ public class OwnerController {
         model.addAttribute("userCommand", new UserCommand());
         return "user/owner/signup-form";
     }
-
-    public static String[] PUBLIC_URLS = {
-            "/owner/signup"
-    };
 }
