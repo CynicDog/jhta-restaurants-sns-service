a8365464f772abef211cdd4178e7b03d1c4e888f
GoTaeHwa <koteha@naver.com>
2023-08-18 17:50:19 +0900

    register update

--- src/main/java/kr/co/jhta/restaurants_service/service/PostService.java
+++ src/main/java/kr/co/jhta/restaurants_service/service/PostService.java
@@ -3,6 +3,8 @@ package kr.co.jhta.restaurants_service.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import kr.co.jhta.restaurants_service.controller.command.PostDataCommand;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 
 import kr.co.jhta.restaurants_service.mapper.PostMapper;
@@ -33,12 +37,12 @@ public class PostService {
 
 	private final Logger logger = Logger.getLogger(PostService.class);
 
-    private final Storage storage;
 	private final PostMapper postmapper;
 	private final StoreMapper storeMapper;
 	private final PostDataMapper postDataMapper;
 	private final PostCommentMapper postCommentMapper;
 	private final PostRepository postRepository;
+	private final Storage storage;
 
 	public List<Post> getAllPosts(){
 		List<Post> posts = postmapper.getAllPosts();
@@ -48,10 +52,24 @@ public class PostService {
 	public void insertPost(Post post, List<PostDataCommand> postDataCommands) throws IOException {
 
 		postmapper.insertPost(post);
+		
+		BlobId id = BlobId.of("jhta-restaurant-service", "REPACE_WITH_FILE_NAME");
+        BlobInfo blobInfo = BlobInfo.newBuilder(id).build();
 
 		postDataCommands.stream()
 				.map(postDataCommand -> {
 					Store store = storeMapper.getStoreById(postDataCommand.getStoreId());
+					
+					File file = new File("REPACE_WITH_FILE_LOCATION");
+
+			        byte[] bytes;
+					try {
+						bytes = Files.readAllBytes(Paths.get(file.toURI()));
+						storage.create(blobInfo, bytes);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
 					return new PostData(post, store, postDataCommand.getContent(), postDataCommand.getChooseFile().getOriginalFilename());
 				})
 				.forEach(postData -> {
