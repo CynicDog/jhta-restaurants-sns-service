715100169b30f7f68948817d83d6208cfe35de71
ginsenglee <thecynicdog0328@gmail.com>
2023-08-28 21:09:02 +0900

    style

--- src/main/java/kr/co/jhta/restaurants_service/repository/FollowRequestRepository.java
+++ src/main/java/kr/co/jhta/restaurants_service/repository/FollowRequestRepository.java
@@ -2,6 +2,7 @@ package kr.co.jhta.restaurants_service.repository;
 
 import kr.co.jhta.restaurants_service.vo.socials.FollowRequest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,6 @@ public interface FollowRequestRepository extends JpaRepository<FollowRequest, In
 
     Page<FollowRequest> findBySenderIdOrderByCreateDateDesc(int senderId, Pageable pageable);
     Page<FollowRequest> findByRecipientIdOrderByCreateDateDesc(int recipientId, Pageable pageable);
+
+    Page<FollowRequest> findByRecipientIdAndStatusOrderByCreateDate(Integer customerId, FollowRequest.RequestStatus requestStatus, PageRequest of);
 }
